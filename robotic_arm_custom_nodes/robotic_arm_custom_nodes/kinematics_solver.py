e90c84f7ba70fb1ce2a56370813c8596cbea4143
newtonjeri <newtonkaris45@gmail.com>
2023-11-12 01:36:29 +0300

    minor changes in

--- robotic_arm_custom_nodes/robotic_arm_custom_nodes/kinematics_solver.py
+++ robotic_arm_custom_nodes/robotic_arm_custom_nodes/kinematics_solver.py
@@ -2,12 +2,15 @@
 import os
 import ikpy.chain
 
-urdf_file="/home/newtonjeri/ai_based_sorting_robot_arm/src/AI-SortingRobotArm/robotic_arm_description/urdf/robotic_arm_urdf.urdf"
+urdf_file="/home/newton/ROS2/ai_sorting_robotic_arm/src/AI-SortingRobotArm/robotic_arm_description/urdf/robotic_arm_urdf.urdf"
 
-robotic_arm = ikpy.chain.Chain.from_urdf_file(urdf_file, active_links_mask=[False, False, True, True, True, True, False])
+robotic_arm = ikpy.chain.Chain.from_urdf_file(urdf_file)
 T=robotic_arm.forward_kinematics([0]* 7)
 print("++++++++++++++++++++++++++++++++++++++++++++++++\n")
-print(f"{robotic_arm.links}")
+
+for link in robotic_arm.links:
+    print(f"{robotic_arm.links}")
+    
 print("\n")
 print("++++++++++++++++++++++++++++++++++++++++++++++++\n")
 print("\nTransformation Matrix :\n",T)
